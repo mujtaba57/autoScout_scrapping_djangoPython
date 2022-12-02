35a4af91315118b9e87c6e20b4bccf36a6976aca
Ahmad057 <ahmad.mujtuba@arhamsoft.org>
2022-12-02 18:26:50 +0500

    change detail data file

--- detail_scrape.py
+++ detail_scrape.py
@@ -4,12 +4,14 @@ import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
 from datetime import datetime
 from datetime import datetime
 import pytz
-
+import json
+from scrap_links import scrap_links_only
 import sys, os, django
 
 sys.path.append("/path/to/dataScrapping")
@@ -18,21 +20,14 @@ django.setup()
 
 from scrappingApp.models import CarDetail, CarDataIndex, DetailScrapExecutionTime, AllCarLink
 
-
-time_sta = DetailScrapExecutionTime.objects.all()
-print("******", time_sta)
+execution_time = DetailScrapExecutionTime.objects.filter().values_list("startTime", "endTime")
+start_execution_time, end_execution_time = execution_time[0][0], execution_time[0][1]
 
 all_car_info = []
 driver = ""
 
-est_start_h = 18
-est_start_m = 46
-est_start_s = 0
-
-est_end_h = 19
-est_end_m = 0
-est_end_s = 0
-
+est_start_h, est_start_m, est_start_s = start_execution_time.hour, start_execution_time.minute, start_execution_time.second
+est_end_h, est_end_m, est_end_s = end_execution_time.hour, end_execution_time.minute, end_execution_time.second
 
 
 def current_zone_time():
@@ -40,32 +35,52 @@ def current_zone_time():
 
 
 def convert_into_seconds(hour, minute, second):
-    return hour*3600+minute*60+second
+    return hour * 3600 + minute * 60 + second
 
 
 def end_execution_time():
     current_time = current_zone_time()
     current_time_second = convert_into_seconds(current_time.hour, current_time.minute, current_time.second)
     endtime_in_second = convert_into_seconds(est_end_h, est_end_m, est_end_s)
-    
-    return endtime_in_second - current_time_second
 
+    return endtime_in_second - current_time_second
 
 
 def start_execution_time():
     current_time = current_zone_time()
     current_time_second = convert_into_seconds(current_time.hour, current_time.minute, current_time.second)
     est_start_total_sec = convert_into_seconds(est_start_h, est_start_m, est_start_s)
-    if current_time_second>=est_start_total_sec:
-        return abs(24*3600*60*60 - (est_start_total_sec + current_time_second))   
+    if current_time_second >= est_start_total_sec:
+        return abs(24 * 3600 * 60 * 60 - (est_start_total_sec + current_time_second))
     else:
         return abs(est_start_total_sec - current_time_second)
 
 
+def add_car_data_to_db():
+    df = pd.read_csv(f"./csv_files/data(cleanN).csv")
+    for i in range(0, len(df)):
+        CarDetail.objects.create(
+            carModel=df['name'][i], carMileage=df['mileage'][i], carRegistration=df['first registration'][i],
+            carPower=df['power'][i], carImage=df['image'][i],
+            carGearbox=df['gearbox'][i], carEngine=df['engine size'][i], carGears=df['gears'][i],
+            carFuelType=df['fuel type'][i],
+            carFuelConsumption=df['fuel consumption'][i], carEmissions=df['co₂-emissions'][i],
+            carColor=df['colour'][i], carManColor=df['manufacturer colour'][i], carBodyType=df['body type'][i],
+            carType=df['type'][i],
+            carSeats=df['seats'][i], carDoors=df['doors'][i], countryName=df['country version'][i],
+            carOfferNumber=df['offer number'][i], carModelCode=df['model code'][i],
+            carPreviousOwner=df['previous owner'][i],
+            carrEmissionClass=df['emission class'][i], carNonSmoker=df['non-smoker vehicle'][i],
+            carPrice=df['price'][i], carVAT=df['VAT'][i], carEquipment=df['equipment'][i],
+            carContactName=df['contact_name'][i],
+            carContactNumber=df['contact_number'][i], carContactAddress=df['contact_address'][i],
+            carCompanyName=df['company_name'][i]
+        )
+
 
 def scrap_data(link):
     global driver
-    driver = webdriver.Chrome(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
     car_data = {}
     feature_data = []
     driver.get(link.format())
@@ -83,7 +98,7 @@ def scrap_data(link):
         carname = " ".join(carname.lower().split("\n"))
         car_data["name"] = carname
         carprice = WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.CSS_SELECTOR,
-                                                                                    "#__next > div > div > main > div.StageArea_container__YuNIp > div.StageArea_informationContainer__VaFP8 > div.Price_mainPriceContainer__syzQE > div:nth-child(1) > div.PriceInfo_styledPriceRow__2fvRD > div > span"))).text
+                                                                                     "#__next > div > div > main > div.StageArea_container__YuNIp > div.StageArea_informationContainer__VaFP8 > div.Price_mainPriceContainer__syzQE > div:nth-child(1) > div.PriceInfo_styledPriceRow__2fvRD > div > span"))).text
         car_data["price"], car_data["VAT"] = carprice.split("-")
 
         time.sleep(5)
@@ -97,7 +112,7 @@ def scrap_data(link):
 
         time.sleep(5)
         WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.CSS_SELECTOR,
-                                                                        f"#__next > div > div > main > div.DetailPage_slicesContainer__wHHae > div > div > div.DetailsSection_childrenSection__NQLD7 > button"))).click()
+                                                                          f"#__next > div > div > main > div.DetailPage_slicesContainer__wHHae > div > div > div.DetailsSection_childrenSection__NQLD7 > button"))).click()
         equipment_value = WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.CSS_SELECTOR,
                                                                                             f"#__next > div > div > main > div.DetailPage_slicesContainer__wHHae > div > div > div.DetailsSection_childrenSection__NQLD7 > div > dl > dd > ul"))).text
         car_data["equipment"] = equipment_value.lower()
@@ -105,9 +120,9 @@ def scrap_data(link):
         contactName = WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.CSS_SELECTOR,
                                                                                         f"#__next > div > div > main > div.DetailPage_slicesContainer__wHHae > div.VendorAndCtaSection_container__ivfyv > div > div.VendorData_mainContainer__AWyih > div.VendorData_bodyContainer__sJZZC > div.Contact_container__TYO5v > span.Contact_contactName__MFXhS"))).text
         contactNumber = WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.CSS_SELECTOR,
-                                                                                        f"#__next > div > div > main > div.DetailPage_slicesContainer__wHHae > div.VendorAndCtaSection_container__ivfyv > div > div.VendorData_mainContainer__AWyih > div.VendorData_bodyContainer__sJZZC > div.Contact_container__TYO5v > div > a"))).text
+                                                                                          f"#__next > div > div > main > div.DetailPage_slicesContainer__wHHae > div.VendorAndCtaSection_container__ivfyv > div > div.VendorData_mainContainer__AWyih > div.VendorData_bodyContainer__sJZZC > div.Contact_container__TYO5v > div > a"))).text
         contactAddress = WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.CSS_SELECTOR,
-                                                                                        f"#__next > div > div > main > div.DetailPage_slicesContainer__wHHae > div.VendorAndCtaSection_container__ivfyv > div > div.VendorData_mainContainer__AWyih > div.VendorData_bodyContainer__sJZZC > div.Department_openingHoursContainer__VP9fd > div.Department_departmentContainer__uYING > a > div"))).text
+                                                                                           f"#__next > div > div > main > div.DetailPage_slicesContainer__wHHae > div.VendorAndCtaSection_container__ivfyv > div > div.VendorData_mainContainer__AWyih > div.VendorData_bodyContainer__sJZZC > div.Department_openingHoursContainer__VP9fd > div.Department_departmentContainer__uYING > a > div"))).text
         companyName = WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.CSS_SELECTOR,
                                                                                         f"#__next > div > div > main > div.DetailPage_slicesContainer__wHHae > div.VendorAndCtaSection_container__ivfyv > div > div.VendorData_mainContainer__AWyih > div.VendorData_bodyContainer__sJZZC > div.RatingsAndCompanyName_container__9_HA4 > div.RatingsAndCompanyName_dealer__HTXk_ > div:nth-child(1)"))).text
 
@@ -119,7 +134,7 @@ def scrap_data(link):
         for i in range(0, 11):
             try:
                 basic_data = WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.CSS_SELECTOR,
-                                                                                            f"#__next > div > div > main > div.DetailPage_slicesContainer__wHHae > div:nth-child({i}) > div"))).text
+                                                                                               f"#__next > div > div > main > div.DetailPage_slicesContainer__wHHae > div:nth-child({i}) > div"))).text
                 feature_data.append(basic_data)
             except Exception:
                 pass
@@ -133,11 +148,11 @@ def scrap_data(link):
 
     except Exception:
         pass
-        
+
     if car_data != "":
         all_car_info.append(car_data)
         keys = car_data.keys()
-        with open('csv_files/dataN.csv', 'a') as f_object:
+        with open('./csv_files/dataN.csv', 'a') as f_object:
             dictwriter_object = DictWriter(f_object, fieldnames=keys)
             dictwriter_object.writerow(car_data)
             f_object.close()
@@ -147,10 +162,10 @@ def scrap_data(link):
     read_df = _df.copy()
     read_df = read_df[read_df["name"].notnull()]
 
-    read_df['price'] = read_df['price'].str.replace(r'€', '')
-    read_df['price'] = read_df['price'].str.replace(r',', '')
-    read_df['price'] = read_df['price'].str.replace(r'.', '')
-    read_df['price'] = read_df['price'].str.replace(r' ', '')
+    read_df['price'] = read_df['price'].str.replace(r'€', '', regex=True)
+    read_df['price'] = read_df['price'].str.replace(r',', '', regex=True)
+    read_df['price'] = read_df['price'].str.replace(r'.', '', regex=True)
+    read_df['price'] = read_df['price'].str.replace(r' ', '', regex=True)
 
     kw_power = []
     for power in read_df["power"]:
@@ -165,9 +180,9 @@ def scrap_data(link):
 
     read_df["power"] = kw_power
 
-    read_df['mileage'] = read_df['mileage'].str.replace(r'km', '')
-    read_df['mileage'] = read_df['mileage'].str.replace(r',', '')
-    read_df['mileage'] = read_df['mileage'].str.replace(r' ', '')
+    read_df['mileage'] = read_df['mileage'].str.replace(r'km', '', regex=True)
+    read_df['mileage'] = read_df['mileage'].str.replace(r',', '', regex=True)
+    read_df['mileage'] = read_df['mileage'].str.replace(r' ', '', regex=True)
 
     fr_year = []
     for year in read_df["first registration"]:
@@ -182,28 +197,27 @@ def scrap_data(link):
     read_df.to_csv(f"csv_files/data(cleanN).csv")
 
 
-
-
-
 def main_function():
-    car_link_df = pd.read_csv("csv_files/car_links.csv")
+    car_link_df = pd.read_csv("./csv_files/car_links.csv")
 
     while True:
         start_time = start_execution_time()
         if start_time == 1:
-            for index, link in enumerate(car_link_df.car_links):
+            car_link_index = CarDataIndex.objects.filter().values_list("queryIndex")
+            for index, link in enumerate(car_link_df.car_links[:car_link_index[0][0]]):
                 endtime = end_execution_time()
-                if endtime>=1 :
-                    car_data = scrap_data(link)       
+                if endtime >= 1:
+                    scrap_data(link)
                 else:
                     CarDataIndex.objects.update(queryIndex=int(index), modify_time=datetime.now(), id=1)
-                    print("\n",endtime, index, "\n")
                     driver.quit()
+                    add_car_data_to_db()
                     break
-        
+        else:
+            print("link scrapping: ")
+            scrap_links_only()
+
         time.sleep(1)
         print("delay_time: ", start_time)
-    
-   
+
 main_function()
-    
